672213ff63e56c5618ce3b648e213dc2da39d5be
Sciencinfus <35138082+Sciencinfus@users.noreply.github.com>
2022-03-18 18:17:16 +0100

    Add City works as defined Weather is pulled for Open Weather TODO : Display weather current data

--- app.py
+++ app.py
@@ -1,5 +1,58 @@
+from flask import Flask, render_template, request
+from helpers import get_gzipped_json, get_filtered_cities, get_id, get_city, get_current_weather
+
 # Configure application
 app = Flask(__name__)
 
+# Define Openweather API Key
+OPENWEATHER_API_KEY = "2a4682ad87a21f03c7bb1592ebe6b99d"
+
+# Define Openweather list of Cities URI
+OPENWEATHER_CITY = "https://bulk.openweathermap.org/sample/city.list.json.gz"
+
+# Get Openweather cities list
+cities = get_gzipped_json(OPENWEATHER_CITY)
+
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/addcity", methods=["GET", "POST"])
+def addcity():
+    """Add a city in the list"""
+    # POST
+    if request.method == "POST":
+
+        # Recover id from Posted data
+        id = get_id(request.get_json(silent=True))
+
+        # Get City data
+        city = get_city(id, cities)
+
+        # Get City Weather
+        weather = get_current_weather(city, OPENWEATHER_API_KEY)
+        print (weather)
+
+        return render_template("index.html")
+    # GET
+    return render_template("addcity.html")
+
+@app.route("/search")
+def search():
+    q = request.args.get("q")
+    if q:
+        choices = get_filtered_cities(cities, q)
+    else:
+        choices = []
+    return render_template("search.html", choices=choices)
---
+++ helpers.py
@@ -0,0 +1,55 @@
+from gzip import decompress
+from json import loads
+import re
+
+from requests import get
+from urllib.request import urlopen
+import json
+
+
+def get_gzipped_json(url):
+    return loads(decompress(get(url).content))
+
+def get_filtered_cities(cities, q):
+    filtered = []
+    max_to_return = 10
+    number_found = 0
+    for city in cities:
+        if city["name"].startswith(q):
+            value = city["name"]
+            if city["state"]:
+                value += "-" + city["state"]
+            value += "-" + city["country"]
+            value += " (" + str(city["id"]) + ")"
+            filtered.append(value)
+            number_found += 1
+            if number_found == max_to_return:
+                return filtered
+    return filtered
+
+def get_id(j):
+    m = re.search('\((.+)\)',j["selected"])
+    if m:
+        return m.group(1)
+    return ""
+
+def get_city(id, cities):
+    for city in cities:
+        if str(city["id"]) == id:
+            return city
+    return {}
+
+def get_current_weather(city, OPENWEATHER_API_KEY):
+
+    # Extract lat and lon from city
+    lat = str(city["coord"]["lat"])
+    lon = str(city["coord"]["lon"])
+
+    # Prepare URL
+    url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + OPENWEATHER_API_KEY
+
+    # Get response from URL
+    response = urlopen(url)
+
+    # Return in JSON format
+    return json.loads(response.read())
